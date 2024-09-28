1c5ad27756aea50a8335644688c68d4e3caedcb9
STougeiro <sidneytougeiro@gmail.com>
2024-09-27 22:30:23 -0300

    Sessoin config support Session regeneration strategy

--- src/Session.php
+++ src/Session.php
@@ -4,73 +4,95 @@
 
     use STDW\Session\Contract\SessionInterface;
     use STDW\Session\Contract\SessionHandlerInterface;
+    use Throwable;
     use Error;
-
 
     class Session implements SessionInterface
     {
         public function __construct(SessionHandlerInterface $handler)
         {
             if (session_status() == PHP_SESSION_DISABLED) {
-                throw new Error("Session is disabled.");
+                throw new Error("Session not started because it's disabled.");
             }
 
-            /**
-             * SOURCERER
-             */
+            $name = 'PHPSESSID';
+            $session_save_path = session_save_path();
 
-            // $name = config('session.cookie.name');
-            // $save_path = config('session.handling.options.save_path');
+            $cookie_lifetime = 0;
+            $cookie_path = '/';
+            $cookie_domain = '';
+            $cookie_secure = 0;
+            $cookie_samesite = 'Lax';
 
-            // $lifetime = config('session.lifetime.minutes');
-
-            // $cookie_path = config('session.cookie.path');
-            // $cookie_domain = config('session.cookie.domain');
-            // $cookie_secure = config('session.cookie.secure');
-            // $cookie_http_only = config('session.cookie.http_only');
-            // $cookie_same_site = config('session.cookie.same_site');
-
-            // session_name($name);
-            // session_save_path($save_path);
-            // session_cache_expire($lifetime);
-
-            // $cookie_lifetime = config('session.lifetime.expires_on_close') ? 0 : TTL::minutes($lifetime);
-
-            // session_set_cookie_params([
-            //     'lifetime' => $cookie_lifetime,
-            //     'path' => $cookie_path,
-            //     'domain' => $cookie_domain,
-            //     'secure' => $cookie_secure,
-            //     'httponly' => $cookie_http_only,
-            //     'samesite' => $cookie_same_site
-            // ]);
-
-            /**
-             * MAJU
-             */
-
-            // $strict_mode = (bool) config('session.strict_mode') ? 1 : 0;
-
-            // $secure = (bool) config('session.cookie.secure') ? 1 : 0;
-            // $http_only = (bool) config('session.cookie.http_only') ? 1 : 0;
-            // $same_site = config('session.cookie.same_site');
-
-            // ini_set('session.use_strict_mode', $strict_mode);
-
-            // ini_set('session.cookie_secure', $secure);
-            // ini_set('session.cookie_httponly', $http_only);
-            // ini_set('session.cookie_samesite', $same_site);
+            $session_gc_maxlifetime = 1800;
+            $session_gc_probability = 1;
+            $session_gc_divisor = 100;
 
 
-            // $name = config('session.name');
-            // $save_path = config('session.save_path');
-            // $lifetime = config('session.lifetime.minutes');
-            // $cache_limiter = config('session.cache_limiter');
- 
-            // session_name($name);
-            // session_save_path($save_path);
-            // session_cache_expire($lifetime);
-            // session_cache_limiter($cache_limiter);
+            try {
+                $name = config('session.name');
+            } catch (Throwable $e) { }
+
+            try {
+                $session_save_path = config('session.save_path');
+            } catch (Throwable $e) { }
+
+            try {
+                $cookie_lifetime = config('session.cookie.lifetime');
+            } catch (Throwable $e) { }
+
+            try {
+                $cookie_path = config('session.cookie.path');
+            } catch (Throwable $e) { }
+
+            try {
+                $cookie_domain = config('session.cookie.domain');
+            } catch (Throwable $e) { }
+
+            try {
+                $cookie_secure = config('session.cookie.secure');
+            } catch (Throwable $e) { }
+
+            try {
+                $cookie_samesite = config('session.cookie.same_site');
+            } catch (Throwable $e) { }
+
+            try {
+                $session_gc_maxlifetime = config('session.garbage_collector.maxlifetime');
+            } catch (Throwable $e) { }
+
+            try {
+                $session_gc_probability = config('session.garbage_collector.probability');
+            } catch (Throwable $e) { }
+
+            try {
+                $session_gc_divisor = config('session.garbage_collector.divisor');
+            } catch (Throwable $e) { }
+
+
+            ini_set('session.name', $name);
+
+            session_save_path($session_save_path);
+
+            ini_set('session.use_strict_mode', 1);
+            ini_set('session.use_cookies', 1);
+            ini_set('session.use_only_cookies', 1);
+            ini_set('session.use_trans_sid', 0);
+
+            session_set_cookie_params([
+                'lifetime' => $cookie_lifetime,
+                'path' => $cookie_path,
+                'domain' => $cookie_domain,
+                'secure' => $cookie_secure,
+                'httponly' => true,
+                'samesite' => $cookie_samesite,
+            ]);
+
+            ini_set('session.gc_maxlifetime', $session_gc_maxlifetime);
+            ini_set('session.gc_probability', $session_gc_probability);
+            ini_set('session.gc_divisor', $session_gc_divisor);
+
+            session_cache_limiter('nocache');
 
             session_set_save_handler($handler, true);
         }
@@ -80,6 +102,29 @@
         {
             if (session_status() === PHP_SESSION_NONE) {
                 session_start();
+            }
+
+
+            $regeneration = false;
+            $regeneration_time = 1200;
+
+            try {
+                $regeneration = config('session.extra.regeneration');
+            } catch (Throwable $e) { }
+
+            try {
+                $regeneration_time = config('session.extra.regeneration_time');
+            } catch (Throwable $e) { }
+
+
+            if ($regeneration && ! $this->exists('_last_regeneration_')) {
+                $this->set('_last_regeneration_', time());
+            }
+
+            if ($regeneration && time() - $this->get('_last_regeneration_') > $regeneration_time) {
+                session_regenerate_id(true);
+
+                $this->set('_last_regeneration_', time());
             }
         }
 
